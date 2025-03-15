acec64d8bee664069e5489f43fa0677446d68a9f
Jordi Novas <alexandernovas.com@gmail.com>
2025-03-15 17:19:23 -0400

    pidiendole un numero al usuario

---
+++ src/ejemplo/Ejemplo.java
@@ -0,0 +1,16 @@
+package ejemplo;
+
+
+public class Ejemplo {
+
+   
+    public static void main(String[] args) {
+        
+        int numero = 10;
+        numero += 5;
+        
+        System.out.println("EL numero es: " + numero);
+        
+    }
+    
+}
