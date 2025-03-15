45959a7f5ca599051a0560c7b6b0f5a31d5e36f8
Jordi Novas <alexandernovas.com@gmail.com>
2025-03-15 17:36:52 -0400

    cambie de numeros a 23

--- src/ejemplo/Ejemplo.java
+++ src/ejemplo/Ejemplo.java
@@ -7,7 +7,7 @@ public class Ejemplo {
     public static void main(String[] args) {
         
         int numero = 15;
-        numero += 20;
+        numero += 23;
         
         System.out.println("EL numero es: " + numero);
         
